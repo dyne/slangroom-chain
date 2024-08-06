6e98c8e4890d14878fc347e93d2548527a07a49f
matteo-cristino <cristino.matteo97@gmail.com>
2024-08-06 17:08:12 +0200

    fix: try to support json and yaml chains
    ⚠️  WIP

--- src/lib/chain.ts
+++ src/lib/chain.ts
@@ -15,16 +15,14 @@ import { shell } from '@slangroom/shell';
 import { timestamp } from '@slangroom/timestamp';
 import { wallet } from '@slangroom/wallet';
 import { zencode } from '@slangroom/zencode';
-import { execaCommand } from 'execa';
-import YAML from 'yaml';
 
+import { JsonChain } from './jsonChain';
 import type {
-  OnBeforeOrAfter,
-  OnBeforeOrAfterData,
+  JsonSteps,
   Results,
   Step,
-  Steps,
 } from './types';
+import { YamlChain } from './yamlChain';
 
 const slang = new Slangroom(
   db,
@@ -43,8 +41,6 @@ const slang = new Slangroom(
   zencode,
 );
 
-/* c8 ignore next */
-const AsyncFunction = async function () {}.constructor;
 
 const readFromFile = (path: string): string => {
   return fs.readFileSync(path).toString('utf-8');
@@ -53,22 +49,6 @@ const readFromFile = (path: string): string => {
 const verbose = (verbose: boolean | undefined): ((m: string) => void) => {
   if (verbose) return (message: string) => console.log(message);
   return () => {};
-};
-
-const execJsFun = async (
-  stringFn: string,
-  args: Record<string, string>,
-): Promise<string> => {
-  const fn = AsyncFunction(...Object.keys(args), stringFn);
-  try {
-    return await fn(...Object.values(args));
-  } catch (e) {
-    throw new Error(`Error executing JS function:\n${stringFn}\n${e}`);
-  }
-};
-
-const execShellCommand = async (command: string): Promise<void> => {
-  await execaCommand(command);
 };
 
 const getDataOrKeys = (
@@ -92,50 +72,37 @@ const getDataOrKeys = (
   return data;
 };
 
-const manageTransform = async (
-  transformFn: string | undefined,
-  transformData: { data: string } | { keys: string },
-  verboseFn: (m: string) => void,
-): Promise<string> => {
-  if (!transformFn) {
-    if ('data' in transformData) return transformData.data;
-    else return transformData.keys;
+const getDataAndKeys = (
+  step: Step,
+  results: Results,
+) : { data: string; keys: string } => {
+  return {
+    data: getDataOrKeys(step, results, 'data'),
+    keys: getDataOrKeys(step, results, 'keys'),
   }
-  const data = await execJsFun(transformFn, transformData);
-  verboseFn(`TRANSFORMED DATA: ${data}`);
-  return data;
-};
-
-const manageBeforeOrAfter = async (
-  stepOnBeforeOrAfter: OnBeforeOrAfter | undefined,
-  data: OnBeforeOrAfterData,
-): Promise<void> => {
-  if (stepOnBeforeOrAfter) {
-    if (stepOnBeforeOrAfter.jsFunction)
-      await execJsFun(stepOnBeforeOrAfter.jsFunction, data);
-    if (stepOnBeforeOrAfter.run)
-      await execShellCommand(stepOnBeforeOrAfter.run);
-  }
-  return;
-};
+}
 
 export const execute = async (
-  steps: string,
+  steps: string | JsonSteps,
   inputData?: string,
 ): Promise<string> => {
   const results: Results = {};
   let final = '';
   let firstIteration = true;
-  const jsonSteps = YAML.parse(steps) as Steps;
-  const verboseFn = verbose(jsonSteps.verbose);
-  for await (const step of jsonSteps.steps) {
-    let data = getDataOrKeys(step, results, 'data');
+  let parsedSteps;
+  if (typeof steps === 'string') parsedSteps = new YamlChain(steps);
+  else parsedSteps = new JsonChain(steps);
+  const verboseFn = verbose(parsedSteps.steps.verbose);
+  for (const step of parsedSteps.steps.steps) {
+    let { data, keys } = getDataAndKeys(step, results);
+    // TODO: remove firstIteration boolean variable and
+    // each time the data is input take as data the result of
+    // previous step for easier chaining
     if (firstIteration) {
       if (data === '{}' && inputData) data = inputData;
       firstIteration = false;
     }
-    let keys = getDataOrKeys(step, results, 'keys');
-    const conf = step.conf ? step.conf : jsonSteps.conf;
+    const conf = step.conf ? step.conf : parsedSteps.steps.conf;
     const zencode =
       'zencodeFromFile' in step
         ? readFromFile(step.zencodeFromFile)
@@ -143,9 +110,9 @@ export const execute = async (
     verboseFn(
       `Executing contract ${step.id}\nZENCODE: ${zencode}\nDATA: ${data}\nKEYS: ${keys}\nCONF: ${conf}`,
     );
-    data = await manageTransform(step.dataTransform, { data }, verboseFn);
-    keys = await manageTransform(step.keysTransform, { keys }, verboseFn);
-    await manageBeforeOrAfter(step.onBefore, { zencode, data, keys, conf });
+    data = await parsedSteps.manageTransform(step.dataTransform, data, 'data', verboseFn)
+    keys = await parsedSteps.manageTransform(step.keysTransform, keys, 'keys', verboseFn);
+    await parsedSteps.manageBefore(step.onBefore, zencode, data, keys, conf);
     const { result, logs } = await slang.execute(zencode, {
       data: JSON.parse(data),
       keys: JSON.parse(keys),
@@ -158,13 +125,13 @@ export const execute = async (
       // this should be unreachable
       throw new Error(`failed to stringify result: ${result}\ngot error: ${e}`);
     }
-    await manageBeforeOrAfter(step.onAfter, {
-      result: stringResult,
+    await parsedSteps.manageAfter(step.onAfter,
+      stringResult,
       zencode,
       data,
       keys,
       conf,
-    });
+    );
     results[step.id] = stringResult;
     verboseFn(logs);
     final = stringResult;
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,98 @@
-export type OnBeforeOrAfter = {
-  readonly run?: string;
-  readonly jsFunction?: string;
+// types for json format
+type JsonOnBeforeFn =
+  | ((
+      zencode: string,
+      data: string | undefined,
+      keys: string | undefined,
+      conf: string | undefined,
+    ) => void)
+  | ((
+      zencode: string,
+      data: string | undefined,
+      keys: string | undefined,
+      conf: string | undefined,
+    ) => Promise<void>);
+
+export type JsonOnBefore =
+  | JsonOnBeforeFn
+  | {
+      jsFunction?: JsonOnBeforeFn,
+      run?: string,
+    };
+
+type JsonOnAfterFn =
+  | ((
+      result: string,
+      zencode: string,
+      data: string | undefined,
+      keys: string | undefined,
+      conf: string | undefined,
+    ) => void)
+  | ((
+      result: string,
+      zencode: string,
+      data: string | undefined,
+      keys: string | undefined,
+      conf: string | undefined,
+    ) => Promise<void>);
+
+export type JsonOnAfter =
+  | JsonOnAfterFn
+  | {
+      jsFunction?: JsonOnAfterFn,
+      run?: string,
+    };
+
+export type JsonOnBeforeOrAfter =
+  | JsonOnBefore
+  | JsonOnAfter
+
+type JsonDataTransform =
+  | ((data: string) => string)
+  | ((data: string) => Promise<string>);
+
+type JsonKeysTransform =
+  | ((keys: string) => string)
+  | ((keys: string) => Promise<string>);
+
+export type JsonTransformFn = JsonDataTransform | JsonKeysTransform;
+
+type JsonBasicStep = {
+  readonly id: string;
+  readonly data?: string;
+  readonly dataFromStep?: string;
+  readonly dataFromFile?: string;
+  readonly dataTransform?: JsonDataTransform;
+  readonly keys?: string;
+  readonly keysFromStep?: string;
+  readonly keysFromFile?: string;
+  readonly keysTransform?: JsonKeysTransform;
+  readonly conf?: string;
+  readonly onAfter?: JsonOnAfter;
+  readonly onBefore?: JsonOnBefore;
 };
 
-type BasicStep = {
+export type JsonStep =
+  | (JsonBasicStep & {
+      readonly zencode: string;
+    })
+  | (JsonBasicStep & {
+      readonly zencodeFromFile: string;
+  });
+
+export type JsonSteps = {
+  readonly steps: readonly JsonStep[];
+  readonly conf?: string;
+  readonly verbose?: boolean;
+};
+
+// types for yaml format
+export type YamlOnBeforeOrAfter = {
+  readonly run?: string;
+  readonly jsFunction?: string;
+}
+
+type YamlBasicStep = {
   readonly id: string;
   readonly data?: string;
   readonly dataFromStep?: string;
@@ -14,23 +103,27 @@ type BasicStep = {
   readonly keysFromFile?: string;
   readonly keysTransform?: string;
   readonly conf?: string;
-  readonly onAfter?: OnBeforeOrAfter;
-  readonly onBefore?: OnBeforeOrAfter;
+  readonly onAfter?: YamlOnBeforeOrAfter;
+  readonly onBefore?: YamlOnBeforeOrAfter;
 };
 
-export type Step =
-  | (BasicStep & {
+type YamlStep =
+  | (YamlBasicStep & {
       readonly zencode: string;
     })
-  | (BasicStep & {
+  | (YamlBasicStep & {
       readonly zencodeFromFile: string;
-    });
+  });
 
-export type Steps = {
-  readonly steps: readonly Step[];
+export type YamlSteps = {
+  readonly steps: readonly YamlStep[];
   readonly conf?: string;
   readonly verbose?: boolean;
 };
+
+// types for both
+export type Step = YamlStep | JsonStep;
+export type Steps = YamlSteps | JsonSteps;
 
 export type Results = {
   [x: string]: string;
